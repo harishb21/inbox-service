34e4f5c280e4a89e1b59e69f3fc8c248ae7304db
HarishB <harishb2@citiustech.com>
2021-08-25 17:58:17 +0530

    inbox project added

---
+++ src/test/java/com/ct/inbox/InboxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.inbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
